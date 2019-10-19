178575d2e81c4812f4e0829fb393889da1c8754f
Srinivasan <j.srini06@gmail.com>
2019-10-19 17:15:25 -0400

    Demo

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,5 @@
 class JenkinsDemo{
 public static void main(String args[]){
-  System.out.println("Hurray!! you are done if your output is /"Finished: SUCCESS/"");
+  System.out.println("Hurray!! you are done if your output is \"Finished: SUCCESS\"");
   }
 }
